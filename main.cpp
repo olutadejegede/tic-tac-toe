5704468c19664f0913615022ca7819ec16f5e6d1
Olutade Jegede <joseph@Olutades-MacBook-Pro.local>
2025-11-26 23:32:13 -0500

    Minor Changes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 ////////////////////////////////////////////////////////
@@ -7,22 +8,27 @@ using namespace std;
 // Class: Data Structures & Algorithms                 /
 ////////////////////////////////////////////////////////
 
-
 ////////////////////////////////////////////////////////
-// Structs:                                            /
+// Struct:                                             /
 ////////////////////////////////////////////////////////
 
 struct Player
 {
-string firstName;
-string lastName;
-char piece;
-int wins = 0;
-int losses = 0;
+  string firstName;
+  string lastName;
+  char piece;
+  int wins;
+  int losses;
+
+  Player()
+  {
+    wins = 0;
+    losses = 0;
+  }
 };
 
 ////////////////////////////////////////////////////////
-// Function Prototypes:                                /  
+// Function Prototypes:                                /
 ////////////////////////////////////////////////////////
 
 void startGame(Player &player1, Player &player2, Player *&current, Player *&other);
@@ -40,15 +46,15 @@ char askToPlayAgain();
 // Main function:                                      /
 ////////////////////////////////////////////////////////
 
-int main() 
+int main()
 {
   Player player1, player2;
   Player *current, *other;
   int gamesPlayed = 0, draws = 0;
-  char board[9] = {'1','5','2','8','9','6','4','7','3'};
+  char board[9] = {'1', '5', '2', '8', '9', '6', '4', '7', '3'};
   player1.piece = 'x';
   player2.piece = 'o';
-  
+
   startGame(player1, player2, current, other);
 
   char playAgain;
@@ -57,33 +63,33 @@ int main()
     resetBoard(board);
     displayBoard(board);
     bool endGame = false;
-    
-  while (!endGame)
-  {
-    makeMove(*current,board);
-    endGame = checkforWinner(board, *current, *other, gamesPlayed,draws);
-    if (!endGame)
+
+    while (!endGame)
     {
-      swap(current,other);
+      makeMove(*current, board);
+      endGame = checkforWinner(board, *current, *other, gamesPlayed, draws);
+      if (!endGame)
+      {
+        swap(current, other);
+      }
     }
+
+    displayScoreboard(player1, player2, gamesPlayed, draws);
+    determineStart(current, other, gamesPlayed, draws);
+    playAgain = askToPlayAgain();
   }
-    
-  displayScoreboard(player1, player2, gamesPlayed, draws);
-  determineStart(current, other, gamesPlayed, draws);
-  playAgain = askToPlayAgain();
-  }
-    
+
   while (playAgain == 'y' || playAgain == 'Y');
-  
-  cout << endl << "Thanks for playing!" << endl;
-  
+
+  cout << endl
+       << "Thanks for playing!" << endl;
+
   return 0;
 }
 
 ////////////////////////////////////////////////////////
 // Function Definitions:                               /
 ////////////////////////////////////////////////////////
-
 
 ////////////////////////////////////////////////////////
 // Function that hanldes the start of the game:        /
@@ -118,7 +124,7 @@ void startGame(Player &player1, Player &player2, Player *&current, Player *&othe
       current = &player2;
       other = &player1;
     }
-    else 
+    else
     {
       current = &player1;
       other = &player2;
@@ -129,7 +135,7 @@ void startGame(Player &player1, Player &player2, Player *&current, Player *&othe
 }
 
 ////////////////////////////////////////////////////////
-// Function that resets the board:                     / 
+// Function that resets the board:                     /
 ////////////////////////////////////////////////////////
 
 void resetBoard(char board[9])
@@ -164,7 +170,7 @@ void displayBoard(char board[9])
 // Function to check all alternative winning lines:    /
 ////////////////////////////////////////////////////////
 
-bool possibleWins(char a, char b) 
+bool possibleWins(char a, char b)
 {
   return (a == 'o' && b == 'O') || (a == 'O' && b == 'o') ||
          (a == 'x' && b == 'X') || (a == 'X' && b == 'x') ||
@@ -177,7 +183,7 @@ bool possibleWins(char a, char b)
 
 bool checkLine(int a, int b, int c, char board[9])
 {
-  if (possibleWins(board[a],board[b]) && possibleWins(board[b],board[c]))
+  if (possibleWins(board[a], board[b]) && possibleWins(board[b], board[c]))
   {
     board[a] = toupper(board[a]);
     board[b] = toupper(board[b]);
@@ -193,64 +199,67 @@ bool checkLine(int a, int b, int c, char board[9])
 
 bool checkforWinner(char board[9], Player &current, Player &other, int &gamesPlayed, int &draws)
 {
-    bool win = false;
-  
-    for (int i = 0; i < 3; i ++)
-    {
-      if(checkLine(i, i+3, i+6, board))
-      {
-        win = true;
-      }
-    }
-  
-    for (int i = 0; i < 7; i+=3)
-    {
-      if (checkLine(i, i+1, i+2, board))
-      {
-        win = true;
-      }
-    }
-  
-    if (checkLine(0, 4, 8, board)) win = true;
-    if (checkLine(2, 4, 6, board)) win = true;
+  bool win = false;
 
-    if (win)
+  for (int i = 0; i < 3; i++)
+  {
+    if (checkLine(i, i + 3, i + 6, board))
     {
-        displayBoard(board);
-        cout << "\n" << current.firstName << " has won the game!" << endl;
-
-        current.wins++;
-        other.losses++;
-        gamesPlayed++;
-        return true;
+      win = true;
     }
+  }
 
-    bool draw = true;
-    for (int i = 0; i < 9; ++i)
+  for (int i = 0; i < 7; i += 3)
+  {
+    if (checkLine(i, i + 1, i + 2, board))
     {
-        if (board[i] != 'x' && board[i] != 'o')
-        {
-            draw = false;
-            break;
-        }
+      win = true;
     }
+  }
 
-    if (draw)
-    {
-      displayBoard(board);
-      cout << "\nThe game is a draw!" << endl;
-      draws++;
-      gamesPlayed++;
-      return true;
-    }
+  if (checkLine(0, 4, 8, board))
+    win = true;
+  if (checkLine(2, 4, 6, board))
+    win = true;
 
+  if (win)
+  {
     displayBoard(board);
-    return false;
+    cout << "\n"
+         << current.firstName << " has won the game!" << endl;
+
+    current.wins++;
+    other.losses++;
+    gamesPlayed++;
+    return true;
+  }
+
+  bool draw = true;
+  for (int i = 0; i < 9; ++i)
+  {
+    if (board[i] != 'x' && board[i] != 'o')
+    {
+      draw = false;
+      break;
+    }
+  }
+
+  if (draw)
+  {
+    displayBoard(board);
+    cout << "\nThe game is a draw!" << endl;
+    draws++;
+    gamesPlayed++;
+    return true;
+  }
+
+  displayBoard(board);
+  return false;
 }
 
 ////////////////////////////////////////////////////////
-// Function that displays the scoreboard:              / 
-//////////////////////////////////////////////////////// 
+// Function that displays the scoreboard:              /
+////////////////////////////////////////////////////////
 
 void displayScoreboard(Player &player1, Player &player2, int &gamesPlayed, int &draws)
 {
@@ -270,16 +279,26 @@ int moveIndex(int move)
 {
   switch (move)
   {
-    case 1: return 0;
-    case 5: return 1;
-    case 2: return 2;
-    case 8: return 3;
-    case 9: return 4;
-    case 6: return 5;
-    case 4: return 6;
-    case 7: return 7;
-    case 3: return 8;
-    default: return -1;
+  case 1:
+    return 0;
+  case 5:
+    return 1;
+  case 2:
+    return 2;
+  case 8:
+    return 3;
+  case 9:
+    return 4;
+  case 6:
+    return 5;
+  case 4:
+    return 6;
+  case 7:
+    return 7;
+  case 3:
+    return 8;
+  default:
+    return -1;
   }
 }
 
@@ -301,7 +320,7 @@ void makeMove(Player &current, char board[9])
   else
   {
     cout << "Invalid move (must be between 1-9) Try again." << endl;
-    makeMove(current,board);
+    makeMove(current, board);
   }
 }
 
@@ -311,11 +330,14 @@ void makeMove(Player &current, char board[9])
 
 void determineStart(Player *&current, Player *&other, int gamesPlayed, int draws)
 {
-    if (draws > 0 && gamesPlayed == draws) {
-        swap(current, other);
-    } else {
-        swap(current, other);
-    }
+  if (draws > 0 && gamesPlayed == draws)
+  {
+    swap(current, other);
+  }
+  else
+  {
+    swap(current, other);
+  }
 }
 
 /////////////////////////////////////////////////////////////
@@ -323,8 +345,8 @@ void determineStart(Player *&current, Player *&other, int gamesPlayed, int draws
 /////////////////////////////////////////////////////////////
 char askToPlayAgain()
 {
-    char playAgain;
-    cout << "Do you want to play again? (y/n): ";
-    cin >> playAgain;
-    return playAgain;
+  char playAgain;
+  cout << "Do you want to play again? (y/n): ";
+  cin >> playAgain;
+  return playAgain;
 }
